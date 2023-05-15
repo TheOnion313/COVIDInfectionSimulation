7b756fbe7ffbe7f784f725f40f8bcff993a3aa5c
TheOnion313 <royn77777@gmail.com>
2023-05-15 14:05:35 +0300

    difuision simulation 3d

--- main.py
+++ main.py
@@ -2,33 +2,45 @@ from math import sqrt
 
 import numpy as np
 from matplotlib import pyplot as plt
-
 import matplotlib.animation as animation
 from matplotlib import style
 
-H = 1  # m
-D = 50
-DT = 0.001  # s
+H = 0.5  # m
+D = 1
+DT = 0.1  # s
+SIZE = 10
+X, Y, Z = 0, 1, 2
+SIM_TIME = 10000
 
-
-def dist(loc1, loc2):
-    return sqrt((loc2[0] - loc1[0]) ** 2 + (loc2[1] - loc1[1]) ** 2)
+#
+# def dist(loc1, loc2):
+#     return sqrt((loc2[X] - loc1[X]) ** 2 + (loc2[Y] - loc1[Y]) ** 2 + (loc2[Z] - loc1[Z]) ** 2)
 
 
 def dfdx2(grid: np.ndarray, loc: (int, int)):
-    loc_1 = (min(len(grid) - 1, loc[0] + 1), loc[1])
-    loc_2 = (max(0, loc[0] - 1), loc[1])
-    return (grid[loc_1] - 2 * grid[loc] + grid[loc_2]) / H ** 2
+    loc_1 = (min(len(grid) - 1, loc[X] + 2), loc[Y], loc[Z])
+    loc_2 = (max(0, loc[X] - 2), loc[Y], loc[Z])
+    return (grid[loc_1] - 2 * grid[loc] + grid[loc_2]) / (4 * H ** 2)
 
 
 def dfdy2(grid: np.ndarray, loc: (int, int)):
-    loc_1 = (loc[0], min(len(grid[0]) - 1, loc[1] + 1))
-    loc_2 = (loc[0], max(0, loc[1] - 1))
-    return (grid[loc_1] - 2 * grid[loc] + grid[loc_2]) / H ** 2
+    loc_1 = (loc[X], min(len(grid[0]) - 1, loc[Y] + 2), loc[Z])
+    loc_2 = (loc[X], max(0, loc[Y] - 2), loc[Z])
+    return (grid[loc_1] - 2 * grid[loc] + grid[loc_2]) / (4 * H ** 2)
+
+
+def dfdz2(grid: np.ndarray, loc: (int, int)):
+    loc_1 = (loc[X], loc[Y], min(len(grid[0][0]) - 1, loc[Z] + 2))
+    loc_2 = (loc[X], loc[Y], max(0, loc[Z] - 2))
+    return (grid[loc_1] - 2 * grid[loc] + grid[loc_2]) / (4 * H ** 2)
+
+
+def index_to_loc(loc):
+    return np.array(loc) * H + (-SIZE / 2)
 
 
 def f(grid):
-    return D * np.array([[dfdx2(grid, (x, y)) + dfdy2(grid, (x, y)) for y in range(len(grid[0]))] for x in range(len(grid))])
+    return D * np.array([[[dfdx2(grid, (x, y, z)) + dfdy2(grid, (x, y, z)) + dfdz2(grid, (x, y, z)) for z in range(len(grid[0][0]))] for y in range(len(grid[0]))] for x in range(len(grid))])
 
 
 def euler(grid):
@@ -36,16 +48,15 @@ def euler(grid):
 
 
 def generate_grid(size):
-    return [[1 / (dist((0, 0), (x - size / 2, y - size / 2)) + 0.0001) for y in np.linspace(0, size, int(size // H))]
-            for x in np.linspace(0, size, int(size // H))]
+    grid = np.zeros([int(size // H), int(size // H), int(size // H)])
+    grid[(int(size // H // 2), int(size // H // 2), int(size // H // 2))] = 10
+    return grid
 
 
 def main():
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1, projection="3d")
-    size = 10
-    grid = np.array(generate_grid(size))
-    print(grid)
+    grid = np.array(generate_grid(SIZE))
 
     grid_arc = []
     t_arc = []
@@ -54,26 +65,31 @@ def main():
     style.use('fivethirtyeight')
     ax = fig.add_subplot(1, 1, 1, projection='3d')
 
-    while t < 1:
+    while t < 10:
         grid_arc.append(grid)
         t_arc.append(t)
         t += DT
         grid = euler(grid)
 
     def animate(i):
-        if i >= len(grid_arc):
+        if i >= SIM_TIME:
             ani.pause()
             return
         xs = []
         ys = []
         zs = []
+        cs = []
         for x in range(len(grid)):
             for y in range(len(grid[x])):
-                xs.append(x * H - size / 2)
-                ys.append(y * H - size / 2)
-                zs.append(grid_arc[i][(x, y)])
+                for z in range(len(grid[x][y])):
+                    xs.append(x * H - SIZE / 2)
+                    ys.append(y * H - SIZE / 2)
+                    zs.append(z * H - SIZE / 2)
+                    cs.append(grid_arc[i][(x, y, z)])
+
         ax.clear()
-        ax.scatter(xs, ys, zs)
+        print()
+        ax.scatter(xs, ys, zs, s=np.array(cs) * 100)
 
     ani = animation.FuncAnimation(fig, animate, interval=DT)
 
